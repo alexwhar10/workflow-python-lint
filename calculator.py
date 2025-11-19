4d07d6b5b082dead27843604d65071368c27c6cf
Alex Wharton <awharton2000@gmail.com>
2025-11-19 14:59:37 -0500

    Add calculator functions with tests

--- calculator.py
+++ calculator.py
@@ -2,11 +2,9 @@ def add(a, b):
     """Add two numbers together."""
     return a + b
 
-
 def multiply(a, b):
     """Multiply two numbers."""
     return a * b
-
 
 def divide(a, b):
     """Divide two numbers."""
